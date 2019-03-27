0751490c52d9a11dcc311ac20594c212edf42702
maklas <maklas-a@yandex.ru>
2019-03-27 16:48:01 +0300

    may be fixed pareto fitness function

--- core/src/ru/maklas/genetics/engine/genetics/ParetoFitnessFunction.java
+++ core/src/ru/maklas/genetics/engine/genetics/ParetoFitnessFunction.java
@@ -27,20 +27,16 @@ public class ParetoFitnessFunction implements FitnessFunction {
         double val_1 = f1.f(x, y);
         double val_2 = f2.f(x, y);
 
-        int better1 = 0;
-        int better2 = 0;
+        int better = 0;
 
         for (Entity entity : generation) {
             if (entity == chromosome) continue;
             Chromosome ec = entity.get(M.chromosome).chromosome;
-            if (f1.f(ec.get(GeneNames.X).decodeAsDouble(), y) < val_1){
-                better1++;
-            }
-            if (f2.f(x, ec.get(GeneNames.Y).decodeAsDouble()) < val_2) {
-                better2++;
+            if (f1.f(ec.get(GeneNames.X).decodeAsDouble(), y) < val_1 && f2.f(x, ec.get(GeneNames.Y).decodeAsDouble()) < val_2){
+                better++;
             }
         }
 
-        return 1.0 / (Math.pow(1.0 + (Math.min(better1, better2) / (generation.size - 1.0)), q));
+        return 1.0 / (Math.pow(1.0 + (better / (generation.size - 1.0)), q));
     }
 }
